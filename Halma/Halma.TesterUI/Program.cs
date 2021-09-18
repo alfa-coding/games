97a942e41bc61a587d86aff7e0ee04fad492ea28
DimasVeliz <trevor90094@gmail.com>
2021-09-18 22:05:23 +0200

    console tester built

--- Halma/Halma.TesterUI/Program.cs
+++ Halma/Halma.TesterUI/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Halma.Logic;
 
 namespace Halma.TesterUI
 {
@@ -6,7 +7,67 @@ namespace Halma.TesterUI
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            GameLogic game = new GameLogic(6,new string[]{"Dimas", "Alejandra"});
+            while (!game.IsFinished)
+            {
+                DrawBoard(game.Board);
+                var pieceToMove = AskForPieceOrPosition(true);
+                //Saber si esta pieza PERTENECE al JUGADOR en TURNO.
+                bool belongs = game.BelongToPlayerInTurn(pieceToMove);
+                if (!belongs)
+                {
+                    Console.WriteLine("The selected piece is NOT yours");
+                }
+                else
+                {
+                    var newPosition = AskForPieceOrPosition(false);
+                    bool wasPosible = game.TryMove(pieceToMove, newPosition);
+                    if (!wasPosible)
+                    {
+                        Console.WriteLine("This movement can not be performed");
+                    }
+                    else
+                    {
+                        int possibleWinner = game.CheckForWinners();
+                        if (possibleWinner != -1)
+                        {
+                            Console.WriteLine($"Player {possibleWinner} won the game");
+
+                        }
+                    }
+
+                }
+
+            }
+            Console.WriteLine("Thank you!");
+            
+        }
+
+      
+
+        private static Pair AskForPieceOrPosition(bool forPiece)
+        {
+            string message = forPiece ? "piece" : "position";
+
+            Console.WriteLine($"Choose the {message}, in the following format : r , c ");
+            var line = Console.ReadLine();
+            var splitted = line.Split(',');
+            int r = int.Parse(splitted[0]);
+            int c = int.Parse(splitted[1]);
+            Pair p = new Pair(r, c);
+            return p;
+        }
+
+        private static void DrawBoard(Board board)
+        {
+
+            for (int i = 0; i < board.Size; i++)
+            {
+                for (int j = 0; j < board.Size; j++)
+                {
+                    Console.WriteLine();
+                }
+            }
         }
     }
 }
