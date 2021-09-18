da75c16261a1cddbb075fbcca29f4206ada2194f
DimasVeliz <trevor90094@yandex.com>
2021-09-18 19:21:54 +0200

    Updating the Utility class

--- Halma/Halma.Logic/Utilities/Utilities.cs
+++ Halma/Halma.Logic/Utilities/Utilities.cs
@@ -1,13 +1,134 @@
-using System;
 using System.Collections.Generic;
 
 namespace Halma.Logic
 {
     public class Utilities
     {
-        public static void InitializePositions(List<Pair> elements)
+        private static bool IsCloseNeighborg(Pair initialPosition, Pair destination, int[] dirX, int[] dirY, int boardSize)
         {
-            
+
+
+            for (int i = 0; i < dirX.Length; i++)
+            {
+                var newRowPosition = initialPosition.Row + dirY[i];
+                var newColPosition = initialPosition.Col + dirX[i];
+
+                if (InRange(newRowPosition, newColPosition, boardSize) && newColPosition == destination.Col && newRowPosition == destination.Row)
+                {
+                    return true;
+                }
+
+            }
+            return false;
         }
+
+        internal static void FillBoard(List<Player> players, Board board)
+        {
+            CleanBoard(board);
+            string[] idenfiers = {"A","B"};
+            for (int i = 0; i < players.Count; i++)
+            {
+                foreach (var piece in players[i].Pieces)
+                {
+                    board[piece.Row,piece.Col] = idenfiers[i];
+                }
+            }
+        }
+
+        private static void CleanBoard(Board board)
+        {
+            for (int i = 0; i < board.Size; i++)
+            {
+                for (int j = 0; j < board.Size; j++)
+                {
+                    board[i,j]=null;
+                }
+            }
+        }
+
+        private static bool InRange(int newRowPosition, int newColPosition, int boardSize)
+        {
+            return (newColPosition >= 0 && newColPosition < boardSize)
+                    && (newRowPosition >= 0 && newRowPosition < boardSize);
+        }
+
+        public static bool BFS(Pair initialPosition, Pair destination, Board board)
+        {
+            //directions=  w,  nw, n, ne, w, se, s, sw
+            int[] dirX = { -1, -1, 0, 1, 1, 1, 0, -1 };
+            int[] dirY = { 0, -1, -1, -1, 0, 1, 1, 1 };
+
+            Queue<Pair> queue = new Queue<Pair>();
+            queue.Enqueue(destination);
+            while (queue.Count != 0)
+            {
+
+                var current = queue.Dequeue();
+
+                if (GotThere(current,destination))
+                {
+                    return true;
+                }
+                //quizas el destino es mi vecino cercano.
+                if (IsCloseNeighborg(current, destination, dirX, dirY, board.Size))
+                {
+                    return true;
+                }
+                //quizas pueda llegar al destino, a traves de algun vecino lejano
+                else
+                {
+                    for (int i = 0; i < dirX.Length; i++)
+                    {
+                        //posiciones del vecino cercano.
+                        var newRowPosition = current.Row + dirY[i];
+                        var newColPosition = current.Col + dirX[i];
+
+                        if (InRange(newRowPosition, newColPosition, board.Size)&&IsFarNeighborg(newColPosition,newRowPosition, board, dirY[i],dirX[i]))
+                        {
+                            //meter en la cola, a la casilla del vecino lejano, que este en la misma direccion del vecino cercano
+                            queue.Enqueue(new Pair(newRowPosition+dirY[i],newColPosition+dirX[i]));
+                        }
+                    }
+                }
+            }
+            return false;
+        }
+
+        private static bool IsFarNeighborg(int newColPosition, int newRowPosition, Board board, int dirY, int dirX)
+        {
+            //si la casilla vecina esta vacia, no es vecino lejano
+            if (board[newRowPosition,newColPosition]==null)
+            {
+                return false;
+            }
+            //si a pesar de que la casilla vecina no este vacia, y, la lejana esta ocupada
+            if (board[newRowPosition+dirY,newColPosition+dirX]!=null)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        private static bool GotThere(Pair current, Pair destination)
+        {
+            return current.Row==destination.Row && current.Col==destination.Col;
+        }
+
+        public static void InitializePosition(List<Player> players, int sizeBoard)
+        {
+            for (int i = 0; i < sizeBoard - 3; i++)
+            {
+                for (int j = 0; j < sizeBoard - 3 - i; j++)
+                {
+                  //System.Console.WriteLine($"{i},{j} SE OPONE A {sizeBoard-i-1},{sizeBoard-j-1}");
+                  players[0].Pieces.Add(new Pair(i,j));
+                  players[1].Pieces.Add(new Pair(sizeBoard-i-1,sizeBoard-j-1));
+
+                }
+            }
+        }
+
+
     }
 }
