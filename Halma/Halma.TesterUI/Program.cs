19e2db99d1b62f240920c14c4fb5944fda2bf886
DimasVeliz <trevor90094@gmail.com>
2021-09-26 13:16:30 +0200

    test added for multiple scenarios,TODO:correct BFS to satisfy the tests

--- Halma/Halma.TesterUI/Program.cs
+++ Halma/Halma.TesterUI/Program.cs
@@ -8,44 +8,125 @@ namespace Halma.TesterUI
         static void Main(string[] args)
         {
             //RunConsoleGUI();
-            TestBFS_8x8();
+            TestBFS_2x2_closeN_False();
+            TestBFS_5x5_closeN_True();
+            TestBFS_5x5_farN_False();
+            TestBFS_7x7_farN_withStep_False();
+            TestBFS_7x7_farN_withStep_True();
+            
+
+        }
+        private static void TestBFS_2x2_closeN_False()
+        {
+            System.Console.WriteLine("TestBFS_2x2_closeN_False");  
+            Board board = new Board(2);
+            Pair initial = new Pair(0, 0);
+            Pair dest = new Pair(1, 1);
+
+            board[0, 0] = "A";
+            board[1, 1] = "A";
+
+            System.Console.WriteLine(Utilities.BFS(initial, dest, board));
+        }
+        private static void TestBFS_5x5_closeN_True()
+        {
+            System.Console.WriteLine("TestBFS_5x5_closeN_True");
+            Board board = new Board(5);
+            Pair initial = new Pair(2,2);
+            Pair dest = new Pair(1, 3);
+
+            board[0,0]="B";
+            board[1,1]="B";
+            board[2,2]="A";
+            board[3,3]="B";
+
+            System.Console.WriteLine(Utilities.BFS(initial, dest, board));
+
+        }
+        private static void TestBFS_5x5_farN_True()
+        {
+            System.Console.WriteLine("TestBFS_5x5_farN_True");
+            Board board = new Board(5);
+            Pair initial = new Pair(2,2);
+            Pair dest = new Pair(4,4);
+            board[0,0]="B";
+            board[1,1]="B";
+            board[2,2]="A";
+            board[3,3]="B";
+
+            System.Console.WriteLine(Utilities.BFS(initial, dest, board));
+
+
+        }
+        private static void TestBFS_5x5_farN_False()
+        {
+            System.Console.WriteLine("TestBFS_5x5_farN_False");
+            Board board = new Board(5);
+            Pair initial = new Pair(2,2);
+            Pair dest = new Pair(0,0);
+            board[0,0]="B";
+            board[1,1]="B";
+            board[2,2]="A";
+            board[3,3]="B";
+            System.Console.WriteLine(Utilities.BFS(initial, dest, board));
+
+        }
+        private static void TestBFS_7x7_farN_withStep_False()
+        {
+            System.Console.WriteLine("TestBFS_7x7_farN_withStep_False");
+            Board board = new Board(7);
+            Pair initial = new Pair(0, 0);
+            Pair dest = new Pair(1,5);
+            board[0,0]="B";
+            board[1,1]="B";
+            board[2,2]="A";
+            board[3,3]="B";
+            board[1,3]="A";
+            board[3,5]="A";
+            System.Console.WriteLine(Utilities.BFS(initial, dest, board));
+
+        }
+        private static void TestBFS_7x7_farN_withStep_True()
+        {
+            System.Console.WriteLine("TestBFS_7x7_farN_withStep_True");
+            Board board = new Board(7);
+            Pair initial = new Pair(0, 0);
+            Pair dest = new Pair(2,6);
+            board[0,0]="B";
+            board[1,1]="B";
+            board[2,2]="A";
+            board[3,3]="B";
+            board[1,3]="A";
+            board[3,5]="A";
+            System.Console.WriteLine(Utilities.BFS(initial, dest, board));
+
         }
 
-        private static void TestBFS_8x8()
+        private static void TestBFS_8x8_farN_withStep_True()
         {
-            Board board= new Board(8);
-            Pair initial= new Pair(6,7);
-            Pair dest= new Pair(4,1);
+            Board board = new Board(8);
+            Pair initial = new Pair(6, 7);
+            Pair dest = new Pair(4, 1);
 
-            board[3,2]="A";
-            board[1,2]="A";
-            board[1,4]="A";
-            board[1,6]="A";
-            board[3,4]="A";
-            board[3,6]="A";
-            board[5,6]="A";
+            board[3, 2] = "A";
+            board[1, 2] = "A";
+            board[1, 4] = "A";
+            board[1, 6] = "A";
+            board[3, 4] = "A";
+            board[3, 6] = "A";
+            board[5, 6] = "A";
 
 
-            System.Console.WriteLine(Utilities.BFS(initial,dest,board));
-        }
-        private static void TestBFS_3x3()
-        {
-            Board board= new Board(3);
-            Pair initial= new Pair(0,0);
-            Pair dest= new Pair(2,2);
-
-            board[1,1]="A";
-
-            System.Console.WriteLine(Utilities.BFS(initial,dest,board));
+            System.Console.WriteLine(Utilities.BFS(initial, dest, board));
         }
 
         private static void RunConsoleGUI()
         {
-            GameLogic game = new GameLogic(6,new string[]{"Dimas", "Alejandra"});
+            GameLogic game = new GameLogic(6, new string[] { "Dimas", "Alejandra" });
             while (!game.IsFinished)
             {
 
-                DrawBoard(game.Board,game.IndexPlayerInTurn);
+                DrawBoard(game.Board, game.IndexPlayerInTurn);
                 var pieceToMove = AskForPieceOrPosition(true);
                 //Saber si esta pieza PERTENECE al JUGADOR en TURNO.
                 bool belongs = game.BelongToPlayerInTurn(pieceToMove);
@@ -99,22 +180,22 @@ namespace Halma.TesterUI
             {
                 for (int j = 0; j < board.Size; j++)
                 {
-                    string box= board[i,j]!=null?board[i,j]:"0";
-                    if (box=="A")
+                    string box = board[i, j] != null ? board[i, j] : "0";
+                    if (box == "A")
                     {
-                        Console.ForegroundColor= ConsoleColor.Green;
+                        Console.ForegroundColor = ConsoleColor.Green;
 
                     }
-                    else if(box=="B")
+                    else if (box == "B")
                     {
-                        Console.ForegroundColor=ConsoleColor.DarkCyan;
+                        Console.ForegroundColor = ConsoleColor.DarkCyan;
                     }
                     else
                     {
-                        Console.ForegroundColor=ConsoleColor.White;
-                        
+                        Console.ForegroundColor = ConsoleColor.White;
+
                     }
-                    Console.Write(box+" ");
+                    Console.Write(box + " ");
                 }
                 System.Console.WriteLine();
             }
